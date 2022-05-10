c599b5707c7758bd24338742e09b5b842227635d
remarkablejames <lascasasjames85@gmail.com>
2022-05-10 22:27:15 +0200

    basic styling using flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SideBar from "./components/SideBar";
 const App = () => {
   return (
     <Box>
-      <Stack>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
         <SideBar />
         <Feed />
         <RightBar />
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <Box
+      bgcolor="skyblue"
+      flex={1}
+      padding={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      SideBar
+    </Box>
+  );
 };
 
 export default SideBar;

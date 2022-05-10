5d37c846b5eda5cbaccc9db2eed6da428e44e88d
remarkablejames <lascasasjames85@gmail.com>
2022-05-10 22:14:01 +0200

    scaffolding the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
-
+import Feed from "./components/Feed";
+import NavBar from "./components/NavBar";
+import RightBar from "./components/RightBar";
+import SideBar from "./components/SideBar";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box>
+      <Stack>
+        <SideBar />
+        <Feed />
+        <RightBar />
+      </Stack>
+    </Box>
+  );
 };
 
 export default App;
